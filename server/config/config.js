885bfc3da00758b1454c6db8a0f4afb874ff0e41
Martha <marthavg.14@gmail.com>
2020-08-06 14:01:42 -0500

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -18,6 +18,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://strider:kBi4zyBBvqcT9GhZ@cluster0.nrxv1.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
