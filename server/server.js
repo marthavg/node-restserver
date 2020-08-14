000cc1d62cfd96b7f4e452208f7237f6f144bc31
Martha <marthavg.14@gmail.com>
2020-08-13 19:53:12 -0500

    Google Sign-In

--- server/server.js
+++ server/server.js
@@ -2,17 +2,22 @@ require('./config/config');
 
 const express = require('express');
 const mongoose = require('mongoose');
-
+const path = require('path');
 
 const app = express();
 
 const bodyParser = require('body-parser');
+const { dirname } = require('path');
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 
 // parse application/json
 app.use(bodyParser.json())
+
+// habilitar la carpeta public
+app.use(express.static(path.resolve(__dirname, '../public')));
+
 
 // Configuración global de rutas
 app.use(require('./routes/index'));
